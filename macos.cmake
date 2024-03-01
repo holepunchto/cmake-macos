1bb19687c1a5d940332e9e1eb5d3283d9a97ca06
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-03-01 12:09:53 +0100

    Add `RESOURCES` argument

--- macos.cmake
+++ macos.cmake
@@ -1,8 +1,20 @@
 set(macos_module_root ${CMAKE_CURRENT_LIST_DIR})
 
 function(add_macos_bundle_info target)
+  set(one_value_keywords
+    DESTINATION
+    NAME
+    VERSION
+    DISPLAY_NAME
+    PUBLISHER_DISPLAY_NAME
+    IDENTIFIER
+    CATEGORY
+    TARGET
+    EXECUTABLE
+  )
+
   cmake_parse_arguments(
-    PARSE_ARGV 1 ARGV "" "DESTINATION;NAME;VERSION;DISPLAY_NAME;PUBLISHER_DISPLAY_NAME;IDENTIFIER;CATEGORY" ""
+    PARSE_ARGV 1 ARGV "" "${one_value_keywords}" ""
   )
 
   if(NOT ARGV_DESTINATION)
@@ -10,6 +22,16 @@ function(add_macos_bundle_info target)
   endif()
 
   cmake_path(ABSOLUTE_PATH ARGV_DESTINATION BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" NORMALIZE)
+
+  if(ARGV_TARGET)
+    set(ARGV_EXECUTABLE $<TARGET_FILE:${ARGV_TARGET}>)
+
+    set(ARGV_EXECUTABLE_NAME $<TARGET_FILE_NAME:${ARGV_TARGET}>)
+  else()
+    cmake_path(ABSOLUTE_PATH ARGV_EXECUTABLE NORMALIZE)
+
+    cmake_path(GET ARGV_EXECUTABLE FILENAME ARGV_EXECUTABLE_NAME)
+  endif()
 
   if(NOT DEFINED ARGV_DISPLAY_NAME)
     set(ARGV_DISPLAY_NAME "${ARGV_NAME}")
@@ -23,8 +45,20 @@ function(add_macos_bundle_info target)
 endfunction()
 
 function(add_macos_bundle target)
+  set(one_value_keywords
+    DESTINATION
+    INFO
+    ICON
+    TARGET
+  )
+
+  set(multi_value_keywords
+    RESOURCES
+    DEPENDS
+  )
+
   cmake_parse_arguments(
-    PARSE_ARGV 1 ARGV "" "DESTINATION;INFO;ICON;TARGET" "DEPENDS"
+    PARSE_ARGV 1 ARGV "" "${one_value_keywords}" "${multi_value_keywords}"
   )
 
   cmake_path(ABSOLUTE_PATH ARGV_DESTINATION BASE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" NORMALIZE)
@@ -54,10 +88,32 @@ function(add_macos_bundle target)
   )
 
   if(ARGV_ICON)
-    list(APPEND commands
-      COMMAND ${CMAKE_COMMAND} -E copy_if_different "${ARGV_ICON}" "${ARGV_DESTINATION}/Contents/Resources/icon.icns"
-    )
+    list(APPEND ARGV_RESOURCES FILE "${ARGV_ICON}" "icon.icns")
   endif()
+
+  while(TRUE)
+    list(LENGTH ARGV_RESOURCES len)
+
+    if(len LESS 3)
+      break()
+    endif()
+
+    list(POP_FRONT ARGV_RESOURCES type from to)
+
+    cmake_path(ABSOLUTE_PATH from NORMALIZE)
+
+    if(type MATCHES "FILE")
+      set(command copy_if_different)
+    elseif(type MATCHES "DIR")
+      set(command copy_directory_if_different)
+    else()
+      continue()
+    endif()
+
+    list(APPEND commands
+      COMMAND ${CMAKE_COMMAND} -E ${command} "${from}" "${ARGV_DESTINATION}/Contents/Resources/${to}"
+    )
+  endwhile()
 
   list(APPEND commands
     COMMAND ${CMAKE_COMMAND} -E copy_if_different "${ARGV_EXECUTABLE}" "${ARGV_DESTINATION}/Contents/MacOS/${ARGV_EXECUTABLE_NAME}"
